f2a4238c22003e817a23d6713ccf8355b389aee7
Dmitriy Stolyarov <nameer@mail.ru>
2021-06-02 14:41:54 +0300

    Вынесли данные в сторону. BLL - business logical layer, UI - user interface

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -16,19 +16,34 @@ const Message = (props) => {
     )
 }
 
+let dialogData = [
+    {id: 1, name: 'Diman'},
+    {id: 2, name: 'Oleg'},
+    {id: 3, name: 'Vera_Ivanovna'},
+    {id: 4, name: 'Zakhar_1994'},
+    {id: 5, name: 'Katya'}
+]
+
+let messageData = [
+    {id: 1, text: 'Hi, my name is Ivan. Where are you from?'},
+    {id: 2, text: 'hello, how many time?'},
+    {id: 3, text: 'you are sun of beach'}
+]
+
 const Dialogs = (props) => {
     return (
         <div className={moduleStyle.dialogs}>
             <div className={moduleStyle.dialogsItem}>
-                <DialogItem name='Dima' id='1'/>
-                <DialogItem name='Oleg' id='2'/>
-                <DialogItem name='Vera_Ivanovna' id='3'/>
-                <DialogItem name='Zakhar_1994' id='4'/>
+                <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
+                <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
+                <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
+                <DialogItem name={dialogData[3].name} id={dialogData[3].id}/>
+                <DialogItem name={dialogData[4].name} id={dialogData[4].id}/>
             </div>
             <div className={moduleStyle.messageItem}>
-                <Message text='Hi, my name is Ivan. Where are you from?'/>
-                <Message text='hello, how many time?'/>
-                <Message text='you are sun of beach'/>
+                <Message text={messageData[0].text}/>
+                <Message text={messageData[1].text}/>
+                <Message text={messageData[2].text}/>
             </div>
         </div>
     );
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,12 @@
 import moduleStyle from './MyPosts.module.css';
 import Post from './Post/Post';
 
+let postsData = [
+    {id: 1, message: 'Hi, how are you to day', likeCount: 15},
+    {id: 2, message: 'This is my firs post?', likeCount: 0},
+    {id: 3, message: 'It\'s second post', likeCount: 4},
+]
+
 const MyPosts = () => {
     return (
         <div className={moduleStyle.myPostsBlock}>
@@ -14,8 +20,9 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={moduleStyle.myPosts}>
-                <Post message='Hi, how are you to day?' likeCount='15'/>
-                <Post message='This is my firs post?' likeCount='27'/>
+                <Post message={postsData[0].message} likeCount={postsData[0].likeCount}/>
+                <Post message={postsData[1].message} likeCount={postsData[1].likeCount}/>
+                <Post message={postsData[2].message} likeCount={postsData[2].likeCount}/>
             </div>
         </div>
     );
