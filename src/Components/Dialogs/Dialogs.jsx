166e237fcf99d056404bfbac52300b7daf04ca85
Dmitriy Stolyarov <nameer@mail.ru>
2021-06-07 11:33:21 +0300

    Научились прокидывать Пропсы. Три раза всё ломалось((.

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,27 +1,39 @@
 import moduleStyle from './Dialogs.module.css';
 import React from 'react'
-import DialogItem from "./DialogItem/DialogsItem";
-import Messages from "./Messages/Messages";
+import {NavLink} from 'react-router-dom';
+
+const DialogItem = (props) => {
+    return(
+        <div className={moduleStyle.dialogs + ' ' + moduleStyle.active}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Messages = (props) => {
+    return (
+        <div className={moduleStyle.message}>{props.text}</div>
+    )
+}
+
+let dialogs = [
+    {id: 1, name: 'Diman'},
+    {id: 2, name: 'Oleg'},
+    {id: 3, name: 'Vera_Ivanovna'},
+    {id: 4, name: 'Zakhar_1994'},
+    {id: 5, name: 'Katya'}
+]
+
+let messages = [
+    {id: 1, text: 'Hi, my name is Ivan. Where are you from?'},
+    {id: 2, text: 'hello, how many time?'},
+    {id: 3, text: 'you are sun of beach'}
+]
+
+let dialogsElement = dialogs.map( dialogs => <DialogItem name={dialogs.name} id={dialogs.id}/>)
+let messagesElements = messages.map( messages => <Messages text={messages.text}/>)
 
 const Dialogs = (props) => {
-
-    let dialogs = [
-        {id: 1, name: 'Diman'},
-        {id: 2, name: 'Oleg'},
-        {id: 3, name: 'Vera_Ivanovna'},
-        {id: 4, name: 'Zakhar_1994'},
-        {id: 5, name: 'Katya'}
-    ]
-
-    let messages = [
-        {id: 1, text: 'Hi, my name is Ivan. Where are you from?'},
-        {id: 2, text: 'hello, how many time?'},
-        {id: 3, text: 'you are sun of beach'}
-    ]
-
-    let dialogsElement = dialogs.map(dialogs => <DialogItem name={dialogs.name} id={dialogs.id}/>)
-    let messagesElements = messages.map(messages => <Messages text={messages.text}/>)
-
     return (
         <div className={moduleStyle.dialogs}>
             <div className={moduleStyle.dialogsItem}>
